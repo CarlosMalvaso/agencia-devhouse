31016259630a426b95ffc2df37f475bc67ef124c
Sophia Malvaso <cnmalvaso@hotmail.com>
2021-07-08 22:02:10 -0300

    criando home e blog.html

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require("express");
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Estamos online");
+    res.sendFile(__dirname + "/views/home.html");
 })
 
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + "/views/home.html");
+})
 
 app.get('/manutencao', (req, res) => {
     res.sendFile(__dirname + "/views/manutencao.html");
 })
 
+app.get('/blog', (req, res) => {
+    res.sendFile(__dirname + "/views/blog.html");
+})
 
 app.listen(3000, () => console.log("servidor DevHouse"));
+
+app.use(express.static('public'));
 
